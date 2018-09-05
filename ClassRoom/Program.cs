deb5d0b5f43e5b9aa93c122bc923230ed9ebd702
Arlind Ukshini <quka93@gmail.com>
2018-09-05 14:26:24 +0200

    add students to classroom

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,6 +10,13 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
-        }
+			KlasseRum klasse = new KlasseRum();
+	        klasse.KlasseNavn = "3B";
+			klasse.SemesterStart = new DateTime(2018, 9, 3);
+
+	        klasse.Klasseliste.Add(new Studerende("Arlind", 4, 24));
+	        klasse.Klasseliste.Add(new Studerende("Benjamin", 6, 18));
+	        klasse.Klasseliste.Add(new Studerende("Zaki", 6, 6));
+		}
     }
 }
