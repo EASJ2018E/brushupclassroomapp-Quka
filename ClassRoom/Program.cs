f1192e78189382c144859f5ad57b03da32f5349a
Arlind Ukshini <quka93@gmail.com>
2018-09-05 14:32:47 +0200

    add students to list

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -14,9 +14,21 @@ namespace ClassRoom
 	        klasse.KlasseNavn = "3B";
 			klasse.SemesterStart = new DateTime(2018, 9, 3);
 
-	        klasse.Klasseliste.Add(new Studerende("Arlind", 4, 24));
-	        klasse.Klasseliste.Add(new Studerende("Benjamin", 6, 18));
-	        klasse.Klasseliste.Add(new Studerende("Zaki", 6, 6));
-		}
+	        klasse.Klasseliste = new List<Studerende>()
+	        {
+		        new Studerende("Arlind", 4, 24),
+		        new Studerende("Benjamin", 6, 18),
+		        new Studerende("Zaki", 6, 6)
+			};
+
+			// Write to console
+	        foreach (Studerende s in klasse.Klasseliste)
+	        {
+		        Console.WriteLine(s);
+	        }
+
+			Console.WriteLine("Press any key to end program");
+	        Console.ReadKey();
+        }
     }
 }
